455511f92112b1d6e99ee846e9e8b5ed0c3e67e8
kevin <kevinpuyol@live.fr>
2017-03-22 14:20:12 +0100

    exo1

---
+++ exo1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>formulaire</title>
+</head>
+<body>
+	<form action="user.php" method="get">
+		<label for='name'>Nom</label>
+		<input type="text" name="name">
+		<label for="firstName">Prénom</label>
+		<input type="text" name="firstName">
+		<input type="submit" name="submit" value="envoyer">
+	</form>
+	<?php 
+
+
+	?>
+</body>
+</html>
